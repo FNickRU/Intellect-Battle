94d2e9e2fa665c18b43df2c111ea421c33624eb2
Monstruos <mongstruos@mail.ru>
2018-08-04 20:45:32 +0700

    hotfix [Task #767967]

--- src/headers/unit.h
+++ src/headers/unit.h
@@ -7,15 +7,13 @@
 
 /**
  * Structure describing questions.
- * @define QUEST_LEN Question string length.
- * @define ANS_LEN   Answer string length.
  * @define ANS_COUNT Number of answers.
  * @param  quest     Question.
  * @param  ans       Array of answers.
  * @param  rans      Number of right answer.
+ * @param  next      Pointer to next unit.
+ * @param  prev      Pointer to previous unit.
  */
-#define QUEST_LEN 240
-#define ANS_LEN 80
 #define ANS_COUNT 4
 struct unit {
     char *quest;
