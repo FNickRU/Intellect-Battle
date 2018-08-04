2a67fd9526e1f31cbaec833823f28066e706e625
Monstruos <mongstruos@mail.ru>
2018-08-04 17:41:53 +0700

    Added interface to room functions [Task #767967]

---
+++ src/headers/player.h
@@ -0,0 +1,20 @@
+/**
+ * Player structure definition.
+ */
+#ifndef __PLAYER_H
+#define __PLAYER_H
+
+
+/**
+ * Structure of player. Workers is creating, rooms is using.
+ * @define USERNAME_LEN Length of player's nickname.
+ * @param  socket       Socket's descriptor for connection with player.
+ * @param  username     Player's nickname.
+ */
+#define USERNAME_LEN 40
+struct player {
+    int socket;
+    char username[USERNAME_LEN];
+};
+
+#endif // __PLAYER_H
